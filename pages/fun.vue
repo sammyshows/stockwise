8628f064d8e4f795a4545117f13c39f13272dfc9
Sam McCarthy <samrmccarthy6@gmail.com>
2022-03-22 01:50:02 +1100

    Testing response

--- pages/fun.vue
+++ pages/fun.vue
@@ -21,12 +21,7 @@ export default defineComponent({
 
   methods: {
     async fetchPrice() {
-      const response = await fetch('/.netlify/functions/price', {
-        method: 'GET',
-        body: JSON.stringify({
-          symbol: "AAPL"
-        })
-      })
+      const response = await fetch('/.netlify/functions/price')
         .then(response => response.json())
 
       this.price = response
