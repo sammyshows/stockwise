4d636cb393e3ab36278233660ab03b1579c6e1df
Sam McCarthy <samrmccarthy6@gmail.com>
2022-03-22 01:52:50 +1100

    Testing response

--- functions/price.js
+++ functions/price.js
@@ -1,11 +1,9 @@
 import fetch from 'node-fetch'
 
 exports.handler = async function (event, context) {
-  const eventBody = JSON.parse(event.body)
+  // const eventBody = JSON.parse(event.body)
 
-  const data = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${eventBody.symbol}&apikey=B642KXFN4VUO0FC2`, {
-    "method": "GET"
-  })
+  const data = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=B642KXFN4VUO0FC2`)
     .then(response => response.json())
     .then(quote => quote["Global Quote"])
 
