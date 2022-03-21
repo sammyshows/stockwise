80739de6783d8defdfe61ac99a8b15bf1fdb126f
Sam McCarthy <samrmccarthy6@gmail.com>
2022-03-21 21:44:24 +1100

    init

---
+++ pages/fun.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Well this is fun!</h1>
+  <p>AAPL price: {{ price }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "Fun",
+
+  created() {
+    this.fetchPrice()
+  },
+
+  data() {
+    return {
+      price: ""
+    }
+  },
+
+  methods: {
+    async fetchPrice() {
+      const response = await fetch('/functions/price')
+          .then(response => response.json())
+
+      this.price = response
+    }
+  }
+})
+</script>
