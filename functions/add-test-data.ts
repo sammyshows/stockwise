4c0382e84cc4e76f7ea7d5dd39d2af88d0449520
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-01 19:15:33 +1100

    Included pg function for testing

--- functions/add-test-data.ts
+++ functions/add-test-data.ts
@@ -1,47 +1,32 @@
 import { Handler } from "@netlify/functions";
-import fetch from "node-fetch";
-// const client = require("../database/client.ts")
-//
-// client.connect();
-//
-// const handler: Handler = async (event, context) => {
-//     return await client
-//         .query('SELECT table_schema,table_name FROM information_schema.tables;')
-//         .then(response => {
-//             for (let row of response.rows) {
-//                 console.log(JSON.stringify(row))
-//             }
-//             client.end()
-//
-//             return {
-//                 statusCode: 200,
-//                 body: JSON.stringify({
-//                     data: 'data'
-//                 })
-//             }
-//         })
-//         .catch(error => {
-//             console.error(error.stack)
-//             client.end()
-//
-//             return {
-//                 statusCode: 400
-//             }
-//         })
-// }
+const client = require("../database/client.ts")
 
-const handler: Handler = async () => {
+client.connect();
 
-    const data = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=B642KXFN4VUO0FC2`)
-        .then(response => response.json())
-        .catch(error => console.error(error))
+const handler: Handler = async (event, context) => {
+    return await client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .then(response => {
+            for (let row of response.rows) {
+                console.log(JSON.stringify(row))
+            }
+            client.end()
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            data: data
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    data: 'data'
+                })
+            }
         })
-    }
+        .catch(error => {
+            console.error(error.stack)
+            client.end()
+
+            return {
+                statusCode: 400
+            }
+        })
 }
 
 export { handler }
