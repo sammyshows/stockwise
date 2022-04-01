6a4999e280bb4a918b666a664d3ed8a69a2792b0
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-01 18:09:33 +1100

    removed pg

--- database/client.ts
+++ database/client.ts
@@ -1,21 +1,21 @@
-const { Client } = require('pg');
-
-const PgClient = (() => {
-    if (process.env.NODE_ENV === 'development') {
-        // If in a 'development' environment allow access WITHOUT a ssl certificate
-        return new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: false
-        })
-    } else {
-        // If in a 'production' environment allow access ONLY WITH a ssl certificate
-        return new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        })
-    }
-})()
-
-module.exports = PgClient
+// const { Client } = require('pg');
+//
+// const PgClient = (() => {
+//     if (process.env.NODE_ENV === 'development') {
+//         // If in a 'development' environment allow access WITHOUT a ssl certificate
+//         return new Client({
+//             connectionString: process.env.DATABASE_URL,
+//             ssl: false
+//         })
+//     } else {
+//         // If in a 'production' environment allow access ONLY WITH a ssl certificate
+//         return new Client({
+//             connectionString: process.env.DATABASE_URL,
+//             ssl: {
+//                 rejectUnauthorized: false
+//             }
+//         })
+//     }
+// })()
+//
+// module.exports = PgClient
--- functions/add-test-data.ts
+++ functions/add-test-data.ts
@@ -1,32 +1,47 @@
 import { Handler } from "@netlify/functions";
-const client = require("../database/client.ts")
+import fetch from "node-fetch";
+// const client = require("../database/client.ts")
+//
+// client.connect();
+//
+// const handler: Handler = async (event, context) => {
+//     return await client
+//         .query('SELECT table_schema,table_name FROM information_schema.tables;')
+//         .then(response => {
+//             for (let row of response.rows) {
+//                 console.log(JSON.stringify(row))
+//             }
+//             client.end()
+//
+//             return {
+//                 statusCode: 200,
+//                 body: JSON.stringify({
+//                     data: 'data'
+//                 })
+//             }
+//         })
+//         .catch(error => {
+//             console.error(error.stack)
+//             client.end()
+//
+//             return {
+//                 statusCode: 400
+//             }
+//         })
+// }
 
-client.connect();
+const handler: Handler = async () => {
 
-const handler: Handler = async (event, context) => {
-    return await client
-        .query('SELECT table_schema,table_name FROM information_schema.tables;')
-        .then(response => {
-            for (let row of response.rows) {
-                console.log(JSON.stringify(row))
-            }
-            client.end()
+    const data = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=B642KXFN4VUO0FC2`)
+        .then(response => response.json())
+        .catch(error => console.error(error))
 
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    data: 'data'
-                })
-            }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: data
         })
-        .catch(error => {
-            console.error(error.stack)
-            client.end()
-
-            return {
-                statusCode: 400
-            }
-        })
+    }
 }
 
 export { handler }
