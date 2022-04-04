358633e1e3132ed8163d5845f57446d04a985a5a
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-04 23:34:07 +1000

    Testing db connection

--- functions/portfolios.ts
+++ functions/portfolios.ts
@@ -4,18 +4,23 @@ const client = require("../database/client.ts")
 
 const handler: Handler = async (event, context) => {
     return await client`
-        SELECT * FROM portfolios;`
+        SELECT table_schema,table_name FROM information_schema.tables;`
         .then(response => {
+            response.forEach(row => {
+                console.log(JSON.stringify(row))
+            })
+            client.end()
 
             return {
                 statusCode: 200,
                 body: JSON.stringify({
-                    data: response
+                    data: 'data'
                 })
             }
         })
         .catch(error => {
             console.error(error.stack)
+            client.end()
 
             return {
                 statusCode: 400
