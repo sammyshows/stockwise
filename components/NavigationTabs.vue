194b05dbc6982dba2f49fa42ac69ff1318dfddc8
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-05 01:15:07 +1000

    Fixed little display bugs - the tiny ones that probably would go unnoticed, but if you did nothing you wouldn't be able to sleep tonight so you stay up fixing it and now ur sleep's fked anyway. Ciao

--- components/NavigationTabs.vue
+++ components/NavigationTabs.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex justify-around mb-5 px-5" aria-label="Tabs">
+  <nav class="flex justify-around mb-5" aria-label="Tabs">
     <NuxtLink v-for="tab in tabConfig.tabs" @click="updateActive(tab.name)" :to="tab.path" :class="[tab.name == activeTab.toUpperCase() ? 'border-cyan-300 text-cyan-300' : 'border-transparent text-gray-500 hover:text-gray-700', 'tracking-widest whitespace-nowrap py-2 px-1 border-b-2 font-medium text-xs']">
       {{ tab.name }}
     </NuxtLink>
--- pages/portfolios/index.vue
+++ pages/portfolios/index.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <div class="flex justify-end gap-3 mb-2 text-xs text-gray-400">
+    <div class="flex justify-end mb-2 text-xs text-gray-400">
       <p class="grow">NAME</p>
-      <p class="w-20 text-right">VALUE/COST</p>
-      <p class="w-16 text-right">DAILY</p>
-      <p class="w-16 text-right">ALL-TIME</p>
+      <p class="w-20 text-right ml-2">VALUE/COST</p>
+      <p class="w-16 text-right ml-2">DAILY</p>
+      <p class="w-16 text-right ml-2">ALL-TIME</p>
     </div>
 
-    <div v-for="portfolio in portfolios" class="flex justify-end gap-3 h-20">
-      <div class="grow truncate">
-        <h2 class="font-bold h-7 tracking-wider truncate">{{ portfolio.name }}</h2>
-        <p class="font-light text-tiny h-4">All-time: <span class="text-bright-green">A$3378.39(87%)</span></p>
-        <p class="font-light text-tiny">All-time: <span class="text-bright-green">A$1722.91(43%)</span></p>
+    <div v-for="portfolio in portfolios" class="mb-5">
+      <div class="flex justify-end">
+        <div class="grow">
+          <h2 class="h-5 w-28 text-sm font-bold tracking-wider truncate">{{ portfolio.name }}</h2>
+          <p class="font-light text-tiny my-0.5 text-gray-300">23 TRANSACTIONS</p>
+        </div>
+        <div class="w-20 text-right mt-0.5 ml-2">
+          <p class="h-5 text-xs font-light">A$6129.87</p>
+          <p class="text-tiny text-gray-500">A$2876.48</p>
+        </div>
+        <div class="w-16 text-right mt-0.5 ml-2">
+          <p class="h-5 text-xs font-light text-bright-red">-A$129.84</p>
+          <p class="text-tiny text-bright-red">-2.07%</p>
+        </div>
+        <div class="w-16 text-right mt-0.5 ml-2">
+          <p class="h-5 text-xs font-light text-bright-green">A$2932.51</p>
+          <p class="text-tiny text-bright-green">80.07%</p>
+        </div>
       </div>
-      <div class="w-20 text-right mt-1">
-        <p class="h-6 text-xs font-light">A$6129.87</p>
-        <p class="text-tiny text-gray-500">A$2876.48</p>
-      </div>
-      <div class="w-16 text-right mt-1">
-        <p class="h-6 text-xs font-light text-bright-red">-A$129.84</p>
-        <p class="text-tiny text-bright-red">-2.07%</p>
-      </div>
-      <div class="w-16 text-right mt-1">
-        <p class="h-6 text-xs font-light text-bright-green">A$2932.51</p>
-        <p class="text-tiny text-bright-green">80.07%</p>
-      </div>
+      <p class="font-light text-tiny h-4">All-time: <span class="text-bright-green">A$3378.39(87%)</span></p>
+      <p class="font-light text-tiny">Realised: <span class="text-bright-green">A$1722.91(43%)</span></p>
     </div>
   </div>
 </template>
