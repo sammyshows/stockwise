0443866c90a9ac5fa615cf5a69c2d9ccc5e1bba1
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-04 15:08:11 +1000

    Stopped ending client

--- functions/portfolios.ts
+++ functions/portfolios.ts
@@ -6,7 +6,6 @@ const handler: Handler = async (event, context) => {
     return await client`
         SELECT * FROM portfolios;`
         .then(response => {
-            client.end()
 
             return {
                 statusCode: 200,
@@ -17,7 +16,6 @@ const handler: Handler = async (event, context) => {
         })
         .catch(error => {
             console.error(error.stack)
-            client.end()
 
             return {
                 statusCode: 400
