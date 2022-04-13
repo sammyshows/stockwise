39af4031894eaf825cd688f03b910441539cb347
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-14 02:01:59 +1000

    Fixed some of create portfolio page formatting

--- pages/portfolios/new.vue
+++ pages/portfolios/new.vue
@@ -1,19 +1,19 @@
 <template>
   <NuxtLayout name="page-container" activeButton="overview">
     <div class="h-full pb-12 flex flex-col">
-      <div class="flex justify-between mb-10">
+      <div class="flex justify-between mb-14">
         <PageTitle :pageDetails="pageDetails" class="truncate mr-3" />
       </div>
 
       <div class="flex flex-col grow justify-between px-6">
-        <div class="flex flex-col grow gap-y-8">
+        <div class="flex flex-col grow gap-y-6">
           <div>
             <label for="name" class="flex items-end">PORTFOLIO NAME</label>
-            <input @click="invalidName = false" v-model="portfolioDetails.name" id="name" type="text" :class="{ 'border-red-600': invalidName }" class="w-full py-4 h-8 bg-transparent border border-0 border-b border-gray-600 rounded-sm text-white">
+            <input @click="invalidName = false" v-model="portfolioDetails.name" id="name" type="text" :class="{ 'border-red-600': invalidName }" class="w-full py-5 h-8 bg-transparent rounded-sm text-white border border-0 border-b border-gray-600 focus:ring-0 focus:border-gray-300" autofocus>
           </div>
           <div class="flex justify-between">
             <label for="included" class="flex items-center">INCLUDED IN TOTALS</label>
-            <input v-model="portfolioDetails.included" id="included" type="checkbox" class="w-8 h-8 text-bright-green bg-transparent rounded-sm duration-100 focus:ring-offset-0 focus:ring-0">
+            <input v-model="portfolioDetails.included" id="included" type="checkbox" class="w-6 h-6 my-auto text-bright-green bg-transparent rounded-sm duration-100 focus:ring-offset-0 focus:ring-0">
           </div>
         </div>
         <div class="text-right">
