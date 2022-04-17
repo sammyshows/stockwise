8871879928e212eaeda59177807c02ee14d75492
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-18 00:02:08 +1000

    Reformatted the add holding page

--- pages/portfolios/[portfolio]/holdings/new.vue
+++ pages/portfolios/[portfolio]/holdings/new.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="h-full flex flex-col">
     <div v-if="$route.path === `/portfolios/${$route.params.portfolio}/holdings/new`">
-      <div class="flex justify-between mb-14">
+      <div class="flex justify-between mb-10">
         <PageTitle :pageDetails="pageDetails" class="truncate mr-3" />
       </div>
 
-      <p class="mb-14 text-center text-lg px-4">What type of holding would you like to add?</p>
-
-      <div class="flex flex-col grow gap-y-10 px-10">
-        <NuxtLink v-for="holdingType in holdingTypes" :to="{ path: `new/${holdingType}` }" class="w-full py-4 border-2 border-bright-cyan bg-opaque-cyan rounded-lg">
-          <h2 class="text-center text-3xl uppercase">{{ holdingType }}</h2>
+      <div class="flex flex-col grow gap-y-10 px-12">
+        <p class="text-sm text-center">What type of holding would you like to add?</p>
+        <NuxtLink v-for="holdingType in holdingTypes" :to="{ path: `new/${holdingType}` }" class="w-full py-3 border-2 border-bright-cyan bg-opaque-cyan rounded-lg">
+          <h2 class="text-center text-xl uppercase">{{ holdingType }}</h2>
         </NuxtLink>
       </div>
     </div>
