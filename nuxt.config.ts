c2a2515d9b6c86e3a9f29da66b156d8226ed86de
Sam McCarthy <samrmccarthy6@gmail.com>
2022-04-26 20:53:04 +1000

    Taking a different approach to making stockwise a PWA since nuxt3 doesn't yet have compatibility with the nuxt/pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,31 +3,12 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: [
-        '@vueuse/nuxt', '@nuxtjs/pwa'
+        '@vueuse/nuxt'
     ],
 
     modules: [
         '@nuxtjs/tailwindcss'
     ],
-
-    pwa: {
-        manifest: {
-            name: 'Stockwise',
-            shortName: 'Stockwise',
-            description: 'An app for studying stocks and tracking your wealth',
-            icons: [
-                {
-                    "src": "https://www.mustakbil.com/content/images/ic_launcher.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
-                }
-            ],
-            start_url: "/",
-            display: 'standalone',
-            background_color: '#000',
-            theme_color: '#000'
-        }
-    },
 
     meta: {
         meta: [
@@ -36,7 +17,8 @@ export default defineNuxtConfig({
         link: [
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
             { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap' }
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap' },
+            { rel: 'manifest', href: 'manifest.json' }
         ]
     },
 
