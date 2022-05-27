ec0a8d3ab2a6b6bbbdf1027251ef77caba8398a3
Sam McCarthy <samrmccarthy6@gmail.com>
2022-05-27 15:06:49 +1000

    Testing auth

--- database/client.ts
+++ database/client.ts
@@ -1,15 +1,15 @@
 import postgres from "postgres";
 
 const PgClient = (() => {
-    if (process.env.DEVELOPMENT) {
-        // If in a 'development' environment allow access WITHOUT a ssl certificate
-        return postgres(process.env.LOCAL_DATABASE_URL)
-    } else {
+    // if (process.env.DEVELOPMENT) {
+    //     // If in a 'development' environment allow access WITHOUT a ssl certificate
+    //     return postgres(process.env.LOCAL_DATABASE_URL)
+    // } else {
         // If in a 'production' environment allow access ONLY WITH a ssl certificate
         return postgres(process.env.DATABASE_URL, {
             ssl: { rejectUnauthorized: false }
         })
-    }
+    // }
 })()
 
 module.exports = PgClient
