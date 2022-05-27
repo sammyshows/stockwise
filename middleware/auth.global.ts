3867c157959a1b62e40a76d9c5ba61956c17502e
Sam McCarthy <samrmccarthy6@gmail.com>
2022-05-28 00:38:07 +1000

    Made auth check work for both testing and production

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,31 +2,36 @@ import createAuth0Client from '@auth0/auth0-spa-js';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     console.log('Starting...')
+    console.log(window.location.href)
 
     const auth0 = await createAuth0Client({
         domain: "stockwise.us.auth0.com",
         client_id: "fkOrDjhrepusnXmq9eWbGFxGl5W4Rm8u",
-        redirect_uri: "https://stockwise.app/portfolios",
+        redirect_uri: window.location.origin === "http://localhost:8888" ? "http://localhost:8888/portfolios" : "https://stockwise.app/portfolios",
         audience: "https://stockwise.app/api"
     });
 
     let isAuthenticated = await auth0.isAuthenticated();
+
     console.log('Authenticated: ' + isAuthenticated)
 
+    // auth0.logout()
     if (!isAuthenticated) {
         const query = to?.query;
         if (query && query.code && query.state) {
             await auth0.handleRedirectCallback();
+            console.log(await auth0.getTokenSilently())
             useState('authToken', async () => await auth0.getTokenSilently())
         } else {
             await auth0.loginWithRedirect();
+            console.log(await auth0.getTokenSilently())
             useState('authToken', async () => await auth0.getTokenSilently())
         }
     } else {
         console.log("logged in ", to.path);
     }
 
-    useState('authToken', async () => await auth0.getTokenSilently())
+    // useState('authToken', async () => await auth0.getTokenSilently())
     navigateTo(to.path);
     console.log('done...')
 });
