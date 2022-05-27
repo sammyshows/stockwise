b9b74b44247f8db45d62e27697ab72330f4b48ac
Sam McCarthy <samrmccarthy6@gmail.com>
2022-05-27 20:14:17 +1000

    Configured auth handling and shared token with app

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,17 +1,20 @@
 import createAuth0Client from '@auth0/auth0-spa-js';
 
-const auth0 = await createAuth0Client({
-    domain: "stockwise.us.auth0.com",
-    client_id: "fkOrDjhrepusnXmq9eWbGFxGl5W4Rm8u",
-    redirect_uri: "http://localhost:8888/portfolios",
-    audience: "https://stockwise.app/api"
-});
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
     console.log('Starting...')
 
-    let isAuthenticated = await auth0.isAuthenticated();
+    const auth0 = await createAuth0Client({
+        domain: "stockwise.us.auth0.com",
+        client_id: "fkOrDjhrepusnXmq9eWbGFxGl5W4Rm8u",
+        redirect_uri: "https://stockwise.app/portfolios",
+        audience: "https://stockwise.app/api"
+    });
+
+    console.log('gday')
+
     useState('authToken', async () => await auth0.getTokenSilently())
+
+    let isAuthenticated = await auth0.isAuthenticated();
     if (to.path === "/" && !to?.query?.code) {
         return;
     }
