09f9015a39fae2683a03da67d2a61185d15a98a8
Sam McCarthy <samrmccarthy6@gmail.com>
2022-06-18 06:32:01 +1000

    Mobile testing chart

--- pages/assets/[symbol]/chart.vue
+++ pages/assets/[symbol]/chart.vue
@@ -1,10 +1,47 @@
 <template>
-  <h1>Chart</h1>
+  <div>
+    <h1>Chart</h1>
+    <canvas ref="chart" id="myChart" width="400" height="400"></canvas>
+  </div>
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from "vue";
+import Chart from 'chart.js/auto';
 
-export default {
-  name: "Asset Chart"
-}
+export default defineComponent({
+  name: "Asset Chart",
+
+  setup() {
+    const chart = ref(null)
+    return { chart }
+  },
+
+  mounted() {
+    new Chart(this.chart, {
+      type: 'line',
+      data: {
+        labels: ['Feb', 'Apr', 'Jun', 'Aug', 'Oct', 'Dec', 'Feb', 'Apr', 'Jun', 'Aug', 'Oct', 'Dec', 'Feb'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40],
+          borderColor: 'rgb(75, 192, 192)'
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            ticks: {
+              maxTicksLimit: 4
+            }
+          }
+        },
+        interaction: {
+          intersect: false,
+          axis: 'x'
+        }
+      }
+    });
+  }
+})
 </script>
