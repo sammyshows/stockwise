1687030ab5f6057a65390a0308b3115b5566942e
Sam McCarthy <samrmccarthy6@gmail.com>
2022-07-15 23:44:33 +1000

    quick fix

--- pages/portfolios/index.vue
+++ pages/portfolios/index.vue
@@ -17,7 +17,7 @@
                   <p class="font-light text-tiny my-0.5 text-gray-300">{{ portfolio.holding_count }} HOLDINGS</p>
                 </div>
                 <div class="w-20 text-right mt-0.5 ml-2 font-normal">
-                  <p class="h-5" :class="$fontSize($formatNumber(portfolio.current_value, 2, true, false) || $formatNumber('0', 0, true, false), 'text-xs', 12)">{{ $formatNumber(portfolio.current_value, 2, true, false)+'g' || $formatNumber('0', 0, true, false) }}</p>
+                  <p class="h-5" :class="$fontSize($formatNumber(portfolio.current_value, 2, true, false) || $formatNumber('0', 0, true, false), 'text-xs', 12)">{{ $formatNumber(portfolio.current_value, 2, true, false) || $formatNumber('0', 0, true, false) }}</p>
                   <p class="text-gray-300" :class="$fontSize($formatNumber(portfolio.initial_value, 2, true, false) || $formatNumber('0', 0, true, false), 'text-tiny', 15)">{{ $formatNumber(portfolio.initial_value, 2, true, false) || $formatNumber('0', 0, true, false) }}</p>
                 </div>
                 <div class="w-16 text-right mt-0.5 ml-2 font-normal" :class="{ 'text-bright-red': portfolio.daily_change < 0, 'text-bright-green': portfolio.daily_change > 0 }">
