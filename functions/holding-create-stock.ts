24f6a6044ce43568bc3152bf984b9b43c6c8499f
Sam McCarthy <samrmccarthy6@gmail.com>
2022-11-27 01:55:42 +1100

    Fix for adding a holding with new asset for the first time to database

--- functions/holding-create-stock.ts
+++ functions/holding-create-stock.ts
@@ -63,9 +63,9 @@ const handler: Handler = requireAuth(async (event, context) => {
 
             holdingId = await client`
                 INSERT INTO holdings (portfolio_id, asset_id)
-                VALUES (${eventBody.portfolio}, ${asset['data'].id})
+                VALUES (${eventBody.portfolio}, ${asset['data']?.id})
                 RETURNING id;`
-                    .then(response => response[0].id)
+                    .then(response => response[0]?.id)
         }
     }
 
