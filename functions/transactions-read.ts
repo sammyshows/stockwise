516537cbaef97133a50d00baa3fcbf0711c3c606
Sam McCarthy <samrmccarthy6@gmail.com>
2022-11-28 01:26:44 +1100

    Moved stored function query to be direct for compatibility with connection pool

--- functions/transactions-read.ts
+++ functions/transactions-read.ts
@@ -8,21 +8,48 @@ const handler: Handler = requireAuth(async (event, context) => {
     let transactions
     let assetData
 
-    const forexs = await client`SELECT * FROM assets WHERE type = 1;`
-
-    await forexs.forEach(async (forex) => {
-        let id = forex.id
-        let symbol = forex.symbol.slice(3, 6)
-
-        await client`
-            UPDATE assets SET currency_id = (
-                SELECT id FROM assets WHERE type = 1 AND symbol = ${symbol + 'USD'}
-            )
-            WHERE id = ${id};
-        `
-    })
-
-    const getTransactions = () => client`SELECT * FROM uspReadTransactions(${eventBody.holdingId})`
+    const getTransactions = () => client`
+        SELECT t.id AS transaction_id,
+               h.id AS holding_id,
+               a.type AS asset_type,
+               SUBSTRING(asset_c.symbol, 1, 3) AS currency_symbol,
+               t.type AS type,
+               t.sell_method AS sell_method,
+               t.exchange_rate AS exchange_rate,
+               t.timestamp AS datetime,
+               t.split_multiplier AS split_multiplier,
+               t.quantity * t.split_multiplier AS initial_quantity,
+               COALESCE(t.quantity * t.split_multiplier - SUM(s.quantity), t.quantity * t.split_multiplier) AS current_quantity,
+               t.initial_price AS price,
+               (t.quantity * t.split_multiplier - COALESCE(SUM(s.quantity), 0)) * t.initial_price / t.split_multiplier * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price) AS initial_value,
+               COALESCE(a.current_price * (t.quantity * t.split_multiplier - SUM(s.quantity)), a.current_price * t.quantity * t.split_multiplier) * asset_c.current_price * user_c.current_price AS current_value,
+               ((a.current_price * asset_c.current_price * user_c.current_price) - (t.initial_price / t.split_multiplier * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price))) * (t.quantity * t.split_multiplier - COALESCE(SUM(s.quantity), 0)) AS total_change,
+               ((a.current_price * (t.quantity * t.split_multiplier - COALESCE(SUM(s.quantity), 0))) - (a.prev_close * (t.quantity * t.split_multiplier - COALESCE(SUM(s.quantity), 0)))) * asset_c.current_price * user_c.current_price AS daily_change,
+               COALESCE(((a.current_price * (t.quantity * t.split_multiplier - SUM(s.quantity))) - (a.prev_close * (t.quantity * t.split_multiplier - SUM(s.quantity)))) * 100.0 / NULLIF(a.prev_close * (t.quantity * t.split_multiplier - SUM(s.quantity)), 0), ((a.current_price * t.quantity * t.split_multiplier) - (a.prev_close * t.quantity * t.split_multiplier))*100.0 / (a.prev_close * t.quantity * t.split_multiplier)) AS daily_percent,
+               CASE t.type
+                   WHEN 0 THEN SUM(s.quantity * (s.sell_price * COALESCE(s.exchange_rate, asset_c.current_price * user_c.current_price) - t.initial_price / t.split_multiplier * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price)))
+                   WHEN 2 THEN t.quantity * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price)
+                   WHEN 3 THEN SUM(s.quantity * (s.sell_price * COALESCE(s.exchange_rate, asset_c.current_price * user_c.current_price)))
+                   END AS realized,
+               CASE t.type
+                   WHEN 0 THEN COALESCE(SUM(s.quantity * (t.initial_price / t.split_multiplier * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price))), 0)
+                   ELSE 0
+                   END AS realized_initial,
+               CASE t.type
+                   WHEN 0 THEN t.initial_value * COALESCE(t.exchange_rate, asset_c.current_price * user_c.current_price)
+                   ELSE 0
+                   END AS all_time_initial
+        FROM transactions as t
+                 INNER JOIN holdings AS h ON t.holding_id = h.id
+                 INNER JOIN assets AS a ON h.asset_id = a.id
+                 INNER JOIN portfolios AS p ON h.portfolio_id = p.id
+                 INNER JOIN user_settings AS u ON p.user_id = u.user_id
+                 INNER JOIN assets AS asset_c ON a.currency_id = asset_c.id
+                 INNER JOIN assets AS user_c ON u.currency_id = user_c.id
+                 LEFT JOIN sells AS s ON t.id = s.transaction_id
+        WHERE h.id = ${eventBody.holdingId}
+        GROUP BY t.id, h.id, a.id, s.transaction_id, asset_c.id, user_c.id
+        ORDER BY MIN(t.timestamp) DESC;`
 
     const getAssetData = () => client`
         SELECT a.id,
