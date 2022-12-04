370e5eda0f98c2fb8c571fa84c96acb092d907db
Sam McCarthy <samrmccarthy6@gmail.com>
2022-12-04 17:07:46 +1100

    Added well known file for deep links

--- plugins/textZoom.ts
+++ plugins/textZoom.ts
@@ -1,7 +1,19 @@
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 import { Capacitor } from '@capacitor/core';
 import { TextZoom } from "@capacitor/text-zoom";
 
 export default defineNuxtPlugin(() => {
+    App.addListener('appUrlOpen', function (event: URLOpenListenerEvent) {
+        // Example url: https://beerswift.app/tabs/tabs2
+        // slug = /tabs/tabs2
+        const slug = event.url.split('.app').pop();
+
+        // We only push to the route if there is a slug present
+        if (slug) {
+            window.location.href = slug
+        }
+    })
+
     if (Capacitor.getPlatform() === 'ios')
     TextZoom.set({
         value: 1.1
