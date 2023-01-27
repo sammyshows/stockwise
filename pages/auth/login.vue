c0015a7820f675e7733adc2c00ee7d54be350389
Sam McCarthy <samrmccarthy6@gmail.com>
2023-01-27 21:17:05 +1100

    Testing capacitor app launcher

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -75,7 +75,7 @@ export default defineComponent({
 
     async idpLogin(identityProvider) {
       // If the user is on the web, go directly to the sign in url.
-      if (this.platform === 'web')
+      if (this.platform === 'web' || this.platform === 'ios')
         window.location.href = `${this.config.AWS_AUTH_URL}/oauth2/authorize?redirect_uri=${this.config.DOMAIN}&response_type=code&client_id=${this.config.AWS_CLIENT_ID}&identity_provider=${identityProvider}&nonce=42466df4-5557-45d0-b4d4-a474dd0a7b6c`
       // If the user is in an App, open a Browser within the app so the user is returned to the app using deep links after signing in.
       else
