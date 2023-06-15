218e56e56fd2ad9cc0947d88efd04aa89acb9852
Sam McCarthy <samrmccarthy6@gmail.com>
2023-06-16 00:00:27 +1000

    Added device app info to letterlock stats upsert

--- functions/letterlock-stats-upsert.ts
+++ functions/letterlock-stats-upsert.ts
@@ -8,15 +8,17 @@ const handler = async (event, context) => {
 
   // Upsert into letterlock_user_stats
   await client`
-    INSERT INTO letterlock_user_stats (user_id, ads_watched_for_lives, ads_watched_for_moves, zero_lives_tally, level_history)
-    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${postgres().json(eventBody.levelHistory)})
+    INSERT INTO letterlock_user_stats (user_id, ads_watched_for_lives, ads_watched_for_moves, zero_lives_tally, level_history, device_os, device_model, stockwise_version)
+    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${postgres().json(eventBody.levelHistory)}, ${eventBody.deviceOS}, ${eventBody.deviceModel}, ${eventBody.stockwiseVersion})
     ON CONFLICT (user_id)
     DO UPDATE SET
         ads_watched_for_lives = EXCLUDED.ads_watched_for_lives,
         ads_watched_for_moves = EXCLUDED.ads_watched_for_moves,
         zero_lives_tally = EXCLUDED.zero_lives_tally,
-        level_history = EXCLUDED.level_history
-  `;
+        level_history = EXCLUDED.level_history,
+        device_os = EXCLUDED.device_os,
+        device_model = EXCLUDED.device_model,
+        stockwise_version = EXCLUDED.stockwise_version`
 
   // Upsert into letterlock_settings
   await client`
@@ -26,8 +28,7 @@ const handler = async (event, context) => {
     DO UPDATE SET
         notifications = EXCLUDED.notifications,
         sound = EXCLUDED.sound,
-        vibrations = EXCLUDED.vibrations
-  `;
+        vibrations = EXCLUDED.vibrations`
 
   return {
     headers: { 'Access-Control-Allow-Origin': '*' },
