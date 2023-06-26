522e801fe619222466d54488dc39cd31a5451e89
Sam McCarthy <samrmccarthy6@gmail.com>
2023-06-26 22:56:49 +1000

    Added letterlock content

---
+++ pages/policies/letterlock/support.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col grow overflow-hidden">
+    <div class="mt-10 h-20 flex justify-between px-3">
+      <PageTitle :pageDetails="pageDetails" class="truncate mr-3" />
+    </div>
+
+    <div class="w-full flex flex-col items-center">
+      <PhoneIcon class="h-16 mb-4 text-bright-cyan" />
+      <p class="mb-4">Email: <span class="text-sm text-bright-cyan">samrmccarthy6@gmail.com</span></p>
+      <p class="px-10 text-xs text-center">
+        At LetterLock we are continuously working to make the game more fun and ensure we are offering the best possible experience we can!
+        <br><br>We encourage you to reach out to us if you have any feedback, feature requests or anything else that you would like to share!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { PhoneIcon } from "@heroicons/vue/outline";
+
+export default defineComponent({
+  name: "Contact Us",
+
+  components: {
+    PhoneIcon
+  },
+
+  data() {
+    return {
+      pageDetails: {
+        title: 'Contact Us',
+        subtitle: 'LETTERLOCK PUZZLE',
+      }
+    }
+  }
+})
+</script>
