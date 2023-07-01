522f3b629495243382b743ae206321e0111617ee
Sam McCarthy <samrmccarthy6@gmail.com>
2023-07-02 00:31:59 +1000

    Bug fix, handle undefined platform

--- functions/letterlock-stats-upsert.ts
+++ functions/letterlock-stats-upsert.ts
@@ -14,7 +14,7 @@ const handler = async (event, context) => {
   // Upsert into letterlock_user_stats
   await client`
     INSERT INTO letterlock_user_stats (user_id, ads_watched_for_lives, ads_watched_for_moves, zero_lives_tally, level_history, device_os, device_model, stockwise_version, platform)
-    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${levelHistoryJson}::jsonb, ${eventBody.deviceOS}, ${eventBody.deviceModel}, ${eventBody.stockwiseVersion}, ${eventBody.platform || 'unknown'}})
+    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${levelHistoryJson}::jsonb, ${eventBody.deviceOS}, ${eventBody.deviceModel}, ${eventBody.stockwiseVersion}, ${eventBody.platform || 'unknown'})
     ON CONFLICT (user_id)
     DO UPDATE SET
         ads_watched_for_lives = EXCLUDED.ads_watched_for_lives,
