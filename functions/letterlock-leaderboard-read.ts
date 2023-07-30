897c983e02f35efbcbcce0545913e3f9346bfd01
Sam McCarthy <samrmccarthy6@gmail.com>
2023-07-31 00:01:06 +1000

    Handled uopdate of user settings letterlock

--- functions/letterlock-leaderboard-read.ts
+++ functions/letterlock-leaderboard-read.ts
@@ -7,24 +7,30 @@ const handler = async (event, context) => {
 
   const leaderboardAllTime = await client`
     WITH ranked_users AS (
-        SELECT 
-            stats.user_id, 
-            stats.device_model, 
-            (SELECT COUNT(*) FROM jsonb_object_keys(CAST(stats.level_history AS jsonb))) - 1 AS levels_completed_count, 
-            stats.updated_at, 
-            stats.created_at,
-            settings.username, 
-            ROW_NUMBER() OVER (ORDER BY ((SELECT COUNT(*) FROM jsonb_object_keys(CAST(stats.level_history AS jsonb))) - 1) DESC) as position
-        FROM letterlock_user_stats AS stats
-        JOIN letterlock_settings AS settings ON stats.user_id = settings.user_id
-        WHERE stats.user_id NOT IN ('81845c27-18fb-4a7b-8fb6-9046c949deb7', '9e5a2c95-4244-4a2a-87bb-3cdb377c67e7')
-    )
-    SELECT user_id, username, device_model, levels_completed_count, updated_at, created_at, position FROM (
-        (SELECT * FROM ranked_users WHERE user_id != ${userId} LIMIT 4)
-        UNION ALL
-        (SELECT * FROM ranked_users WHERE user_id = ${userId})
-    ) as top_users
-    ORDER BY levels_completed_count DESC;`;
+      SELECT 
+          stats.user_id, 
+          stats.device_model, 
+          (SELECT COUNT(*) FROM jsonb_object_keys(CAST(stats.level_history AS jsonb))) - 1 AS levels_completed_count, 
+          stats.updated_at, 
+          stats.created_at,
+          settings.username, 
+          ROW_NUMBER() OVER (
+              ORDER BY 
+                  CASE 
+                      WHEN stats.user_id IN ('81845c27-18fb-4a7b-8fb6-9046c949deb7', '9e5a2c95-4244-4a2a-87bb-3cdb377c67e7', '79eb1e98-9c2f-4133-84cc-584ed8cebef2') THEN 1 
+                      ELSE 0 
+                  END, 
+                  ((SELECT COUNT(*) FROM jsonb_object_keys(CAST(stats.level_history AS jsonb))) - 1) DESC
+          ) as position
+      FROM letterlock_user_stats AS stats
+      JOIN letterlock_settings AS settings ON stats.user_id = settings.user_id
+  )
+  SELECT user_id, username, device_model, levels_completed_count, updated_at, created_at, position FROM (
+      (SELECT * FROM ranked_users WHERE user_id != ${userId} LIMIT 4)
+      UNION ALL
+      (SELECT * FROM ranked_users WHERE user_id = ${userId})
+  ) as top_users
+  ORDER BY levels_completed_count DESC;`;
 
   return {
     headers: { 'Access-Control-Allow-Origin': '*' },
