f910596aaeb09c48079f9971013207dd9cdb4252
Sam McCarthy <samrmccarthy6@gmail.com>
2023-07-31 15:37:31 +1000

    Restored insert part of settings username sql

--- functions/letterlock-stats-upsert.ts
+++ functions/letterlock-stats-upsert.ts
@@ -2,6 +2,7 @@ const client = require("../database/client.ts")
 
 const handler = async (event, context) => {
   const eventBody = JSON.parse(event.body)
+  console.log(eventBody)
 
   const userId = eventBody.settings.id
 
@@ -14,7 +15,7 @@ const handler = async (event, context) => {
   // Upsert into letterlock_user_stats
   await client`
     INSERT INTO letterlock_user_stats (user_id, ads_watched_for_lives, ads_watched_for_moves, zero_lives_tally, level_history, device_os, device_model, letterlock_version, platform)
-    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${levelHistoryJson}::jsonb, ${eventBody.deviceOS}, ${eventBody.deviceModel}, ${eventBody.letterlockVersion || eventBody.stockwiseVersion}, ${eventBody.platform || 'unknown'})
+    VALUES (${userId}, ${eventBody.stats.adsWatchedForLives}, ${eventBody.stats.adsWatchedForMoves}, ${eventBody.stats.zeroLivesTally}, ${levelHistoryJson}::jsonb, ${eventBody.deviceOS}, ${eventBody.deviceModel}, ${eventBody.letterlockVersion || eventBody.stockwiseVersion || null}, ${eventBody.platform || 'unknown'})
     ON CONFLICT (user_id)
     DO UPDATE SET
         ads_watched_for_lives = EXCLUDED.ads_watched_for_lives,
@@ -27,12 +28,20 @@ const handler = async (event, context) => {
         platform = EXCLUDED.platform`
 
   // Upsert into letterlock_settings
+  const usernameEnabled = parseFloat(eventBody.letterlockVersion) >= 2.1 || parseFloat(eventBody.stockwiseVersion) >= 2.1
+
   await client`
     INSERT INTO letterlock_settings (user_id, username, notifications, sound, vibrations)
-    VALUES (${userId}, ${eventBody.settings.username || ''}, ${eventBody.settings.notifications}, ${eventBody.settings.sound}, ${eventBody.settings.vibrations})
+    VALUES (
+        ${userId},
+        ${eventBody.settings.username || ''},
+        ${eventBody.settings.notifications},
+        ${eventBody.settings.sound},
+        ${eventBody.settings.vibrations}
+    )
     ON CONFLICT (user_id)
     DO UPDATE SET
-        username = EXCLUDED.username,
+        username = CASE WHEN ${usernameEnabled} THEN EXCLUDED.username ELSE letterlock_settings.username END,
         notifications = EXCLUDED.notifications,
         sound = EXCLUDED.sound,
         vibrations = EXCLUDED.vibrations`
